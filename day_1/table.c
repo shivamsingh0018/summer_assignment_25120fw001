6fcd96603b82533aac181f3882a5261cb190330e
shivam singh <shivamsingh3.1416@gmail.com>
2026-06-02 22:39:57 +0530

    Initial commit

---
+++ day_1/table.c
@@ -0,0 +1,14 @@
+//program to print table of a given number
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter a number:");
+    scanf("%d", &n);
+
+    for(int i=1; i<=10; i++){
+        printf("%d * %d = %d\n", n,i,n*i);
+    }
+
+    return 0;
+}
